b2b5be441cea302db83e26a659cb812ddb7e01e0
AzmiOzmn <azmiozmen5234@gmail.com>
2024-12-02 22:36:40 +0300

    Add project files.

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcStok.Models.Entity;
+
+namespace mvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        mvcDbStokEntities db = new mvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniKategori() 
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public ActionResult YeniKategori(TBLKATEGORILER p1) 
+        {
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
